1b21d138f1e48d602131ddc77a3b0b5d1f07c8b6
devDabo <52867019+devDabo@users.noreply.github.com>
2023-05-14 16:57:18 +0100

    added mongodb logic
    added logic and mongodb to package.json

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 const express = require('express');
 const port = 4000;
 const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://rajchand99:rajchand99@todo.1e9gpvt.mongodb.net/?retryWrites=true&w=majority";
+
+app.use('/user', userRoutes)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+// mongoose.connect('mongodb+srv://rajchand99:rajchand99@todo.1e9gpvt.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+
 
 app.listen(port, () => console.log('App listening on port 4000'))
