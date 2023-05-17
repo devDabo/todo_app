453c7a2e6fee2a25c4cfbc36412521d0123fc2bf
devDabo <52867019+devDabo@users.noreply.github.com>
2023-05-17 20:22:59 +0100

    added POST route
    can send data from postman using the api route

--- index.js
+++ index.js
@@ -2,29 +2,18 @@ const express = require('express');
 const port = 4000;
 const app = express();
 const mongoose = require('mongoose');
-const createtodo = require('./routes/user');
-//middleware
+const user = require('./routes/user');
+//middleware, so we can parse queries in terminal
 app.use(express.json());
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://rajchand99:rajchand99@todo.1e9gpvt.mongodb.net/?retryWrites=true&w=majority/todo";
 
-//pass query string and route params
-app.all('/test', (req,res) => {
-    // console.log(req.query);
-    // res.send(req.query);
+//use postman to send JSON using sendtodo route
+app.post('/sendtodo', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+})
 
-    //get
-    // console.log(req.params);
-    // res.send(req.params);
-
-    console.log(req.body);
-    res.send(req.body);
-
-});
-
-//post request 
-
-app.use('/createtodo', createtodo)
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -39,10 +28,6 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    //     dbname: 'todo',
-    //     user:'rajchand99',
-    //     pass:'rajchand99'
-    // });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -52,10 +37,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-
-// mongoose.connect('mongodb+srv://rajchand99:rajchand99@todo.1e9gpvt.mongodb.net/?retryWrites=true&w=majority', {useNewUrlParser: true});
 
 
 
--- model/user.js
+++ model/user.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
 //todo schema just consists of a string (for now)
 const userSchema = new Schema({
     todo: {
