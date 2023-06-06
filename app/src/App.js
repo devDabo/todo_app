46bf1d63a22946d4808c961e5b7d893edc81b96f
c1673142 <raj.chand.99@icloud.com>
2023-06-06 12:55:42 +0100

    added app.js
    created react frontend with basic debug button which submits json to mongodb.

---
+++ app/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  onSubmitTodo(){
+    axios.post('http://localhost:4000/createtodo', {
+      todo: 'todo55'
+    })
+    .then(response => {
+      console.log(response.data)
+    })
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <button
+          onClick={()=> this.onSubmitTodo()}
+          >Add todo debug</button>
+      </div>
+    )
+  }
+
+}
+
+export default App;
