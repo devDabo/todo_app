7745857faa9b1ee2e3a96ae5a37cc8d04454f3c3
c1673142 <raj.chand.99@icloud.com>
2023-06-11 11:37:09 +0100

    added frontend capability to make todos

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,30 @@
-import './App.css';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import Form from './components/Form';
 
 class App extends Component {
-  onSubmitTodo(){
-    axios.post('http://localhost:4000/createtodo', {
-      todo: 'todo55'
-    })
-    .then(response => {
-      console.log(response.data)
-    })
-  }
+  onSubmitTodo = () => {
+    axios
+      .post('http://localhost:4000/createtodo', {
+        todo: this.state.todoText
+      })
+      .then(response => {
+        console.log(response.data);
+      });
+  };
+
+  handleTodoTextChange = todoText => {
+    this.setState({ todoText });
+  };
 
   render() {
     return (
-      <div className='App'>
-        <button
-          onClick={()=> this.onSubmitTodo()}
-          >Add todo debug</button>
+      <div className="App">
+        <Form onTodoTextChange={this.handleTodoTextChange} />
+        <button onClick={this.onSubmitTodo}>Add todo</button>
       </div>
-    )
+    );
   }
-
 }
 
 export default App;
