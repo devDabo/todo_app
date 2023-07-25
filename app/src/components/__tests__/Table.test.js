d90773e885a54aab3f7ec35e5a15e1f25da9c041
c1673142 <raj.chand.99@icloud.com>
2023-07-25 12:59:58 +0100

    Update Table.test.js

--- app/src/components/__tests__/Table.test.js
+++ app/src/components/__tests__/Table.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Table from '../Table';
 
-test('renders learn react link', () => {
+test('render todo list on table component', () => {
 render(<Table />);
 const linkElement = screen.getByText(/Todo List/i);
 expect(linkElement).toBeInTheDocument();
