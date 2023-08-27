bb67c71416298a0e60b733fce5465e8e5608ac91
c1673142 <raj.chand.99@icloud.com>
2023-08-27 17:52:28 +0100

    update backend to handle duplicate registrations

--- server/routes/register.js
+++ server/routes/register.js
@@ -7,6 +7,12 @@ router.post('/', async (req, res) => {
   try {
     const { email, password } = req.body;
 
+        // Check if the user with the given email already exists
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+          return res.status(400).json({ error: 'Email already registered' });
+        }
+
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of salt rounds
 
