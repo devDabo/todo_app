d9bac83069b1d1d9de9cd7671001d5154d04af94
c1673142 <raj.chand.99@icloud.com>
2023-09-07 12:55:07 +0100

    added user reference

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Todo = require('../schema/schema');
+const { verifyToken } = require('../middleware/authMiddleware'); // Import the JWT verification middleware
 
-router.post('/', async (req, res) => {
+router.post('/', verifyToken, async (req, res) => {
   try {
     const { todo, complete } = req.body;
+    const userId = req.user.userId; // Get the user ID from the token
+
     const addTodo = new Todo({
       todo: todo,
-      complete: complete || false, // Set the complete field to false by default if not provided in the request
+      complete: complete || false,
+      user: userId, // Associate the todo with the user
     });
 
     const doc = await addTodo.save();
@@ -19,12 +23,15 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', verifyToken, async (req, res) => {
   try {
     const todoId = req.params.id;
+    const userId = req.user.userId; // Get the user ID from the token
+
     console.log(todoId);
 
-    const deletedTodo = await Todo.findByIdAndDelete(todoId);
+    // Delete the todo only if it belongs to the authenticated user
+    const deletedTodo = await Todo.findOneAndDelete({ _id: todoId, user: userId });
 
     if (!deletedTodo) {
       return res.status(404).json({ error: 'Todo not found' });
@@ -38,9 +45,11 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
+router.get('/', verifyToken, async (req, res) => {
   try {
-    const todos = await Todo.find();
+    const userId = req.user.userId; // Get the user ID from the token
+
+    const todos = await Todo.find({ user: userId }); // Fetch todos that belong to the authenticated user
     res.json(todos);
     console.log(todos);
   } catch (err) {
@@ -49,18 +58,20 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', verifyToken, async (req, res) => {
   try {
     const todoId = req.params.id;
+    const userId = req.user.userId; // Get the user ID from the token
     const { todo, complete } = req.body;
 
     const updatedFields = {
       todo: todo,
-      complete: complete || false, // Set the complete field to false by default if not provided in the request
+      complete: complete || false,
     };
 
-    const updatedTodo = await Todo.findByIdAndUpdate(
-      todoId,
+    // Update the todo only if it belongs to the authenticated user
+    const updatedTodo = await Todo.findOneAndUpdate(
+      { _id: todoId, user: userId },
       updatedFields,
       { new: true }
     );
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
   todo: {type: String},
-  complete: {type: Boolean}
+  complete: {type: Boolean},
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to the User schema
+  },
 });
 
 module.exports = mongoose.model('Todo', todoSchema);
